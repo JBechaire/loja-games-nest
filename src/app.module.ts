8b3874899dd16433c16c91cfdd3bf8c5a441c063
Joselaine Bechaire <git.bechaire@gmail.com>
2025-12-09 12:53:17 -0300

    feat: add create folders for classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'loja_games',
+      entities: [],
+      synchronize: true,
+    }),
+    //  lembrar --Other module imports can be added here
+  ],
   controllers: [],
   providers: [],
 })
